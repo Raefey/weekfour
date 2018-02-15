aaeb008bc8c2318b1efd045c9752be40d52b0311
Raefe <raefe.nj@gmail.com>
2018-02-15 16:55:03 +0000

    finishing arrays

--- weekfour/directory.rb
+++ weekfour/directory.rb
@@ -1,31 +1,24 @@
 student_count = 11
 #array of students
 students = [
-   "Dr. Hannibal Lecter"
-   "Darth Vader"
-   "Nurse Ratched"
-   "Michael Corleone"
-   "Alex DeLarge"
-   "The Wicked Witch of the West"
-   "Terminator"
-   "Freddy Krueger"
-   "The Joker"
-   "Joffrey Baratheon"
+   "Dr. Hannibal Lecter",
+   "Darth Vader",
+   "Nurse Ratched",
+   "Michael Corleone",
+   "Alex DeLarge",
+   "The Wicked Witch of the West",
+   "Terminator",
+   "Freddy Krueger",
+   "The Joker",
+   "Joffrey Baratheon",
    "Norman Bates"
  ]
 #now we'll print them
 puts  "The Students of Villains Academy"
 puts  "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-
-puts "Overall, we have #{students.count} great students"
+#puts the students using iteration
+students.each do |student|
+  puts student
+end
+#puts number of students
+puts "Overall we have #{students.count} greats students"
